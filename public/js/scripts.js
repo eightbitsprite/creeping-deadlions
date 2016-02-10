fb193287376144cd7bfcac227d80cce12ee21e8b
Vincent <vincent.yh.huang@gmail.com>
2016-02-10 12:25:15 +0000

    village navigation bar now causes different content to be shown...

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -38,6 +38,8 @@ function initializePage() {
 	$("#menu_log").click(showLog);
 	$("#menu_history").click(showHistory);
 	$("#menu_help").click(showHelp);
+	$("#menu_village").click(showVillage);
+	$("#menu_population").click(showPopulation);
 
 	//Original listeners
 	$("#new_mission_button").click(newMission);
@@ -127,7 +129,26 @@ function showHistory() {
 	$("#help").css("display","none")
 }
 function showHelp() {
-	$("#missions").css("display","none")
-	$("#history").css("display","none")
+	var missions, history, population, village;
+	missions = $("#missions");
+	history = $("#history");
+	population = $("#population");
+	village = $("#village");
+
+	if(missions) {missions.css("display","none");}
+	if(history) {history.css("display","none");}
+	if(population) {population.css("display","none");}
+	if(village) {village.css("display","none");}
+
 	$("#help").css("display","block")
+}
+function showVillage() {
+	$("#village").css("display","block")
+	$("#population").css("display","none")
+	$("#help").css("display","none")
+}
+function showPopulation() {
+	$("#village").css("display","none")
+	$("#population").css("display","block")
+	$("#help").css("display","none")
 }
