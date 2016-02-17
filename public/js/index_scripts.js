46d7f1603d70bf44b3ffb98042e6e4c089e1c5c5
Jordan Yoshihara <jayoshih@ucsd.edu>
2016-02-16 22:31:36 -0800

    Added in databases and login functionality

--- public/js/index_scripts.js
+++ public/js/index_scripts.js
@@ -5,12 +5,9 @@ $(document).ready(function() {
 })
 
 function initializePage() {
-	if(!$("#current_user_data").val() && !window.current_user){
+	if(!window.localStorage.getItem("current_user")){
 		console.log("not logged in");
 		location.replace("/login#sign-in");
-	}else{
-		console.log("current_user:", window.current_user);
-		window.current_user = $("#current_user_data").val();
 	}
 	$("#menu_log").click(showLog);
 	$("#menu_history").click(showHistory);
--- routes/login.js
+++ routes/login.js
@@ -1,19 +1,5 @@
 var data = require('../data.json');
 
 exports.view = function(req, res){
-	if(req.query){
-		var new_user = {
-			"id" : req.query.form_id,
-			"name" : req.query.form_user,
-			"password" : req.query.form_password,
-			"village_level" : 1
-		};
-		data["user"].push(new_user);	
-		console.log("new user is", new_user);
-	}
-	
-	res.render('login', {"id":data["user"].length});
+	res.render('login');
 };
-exports.data = function(req, res) { 
-  	res.json(data['user']);
-}
