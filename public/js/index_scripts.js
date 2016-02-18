6a996c56326b70d841db55c7acbb78606e5ba6f7
k_bright <vincent.yh.huang@gmail.com>
2016-02-17 17:03:01 -0800

    Village.handlebars frankensteined into index.handlebars, navbar now on bottom.

--- public/js/index_scripts.js
+++ public/js/index_scripts.js
@@ -12,29 +12,30 @@ function initializePage() {
 	$("#menu_log").click(showLog);
 	$("#menu_history").click(showHistory);
 	$("#menu_help").click(showHelp);
+	$("#menu_village").click(showVillage);
 } 
 
 function showLog() {
 	$("#missions").css("display","block");
 	$("#history").css("display","none")
+	$("#village").css("display","none")
 	$("#help").css("display","none")
 }
 function showHistory() {
 	$("#missions").css("display","none")
 	$("#history").css("display","block")
+	$("#village").css("display","none")
+	$("#help").css("display","none")
+}
+function showVillage() {
+	$("#missions").css("display","none")
+	$("#history").css("display","none")
+	$("#village").css("display","block")
 	$("#help").css("display","none")
 }
 function showHelp() {
-	var missions, history, population, village;
-	missions = $("#missions");
-	history = $("#history");
-	population = $("#population");
-	village = $("#village");
-
-	if(missions) {missions.css("display","none");}
-	if(history) {history.css("display","none");}
-	if(population) {population.css("display","none");}
-	if(village) {village.css("display","none");}
-
+	$("#missions").css("display","none")
+	$("#history").css("display","none")
+	$("#village").css("display","none")
 	$("#help").css("display","block")
 }
